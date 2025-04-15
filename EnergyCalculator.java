bd80360346d08fe512a452f5ca3abeaef9c266c6
Shaksham Karki <sachyamkarki@gmail.com>
2025-04-15 00:02:17 -0400

    Update EnergyCalculator.java

--- EnergyCalculator.java
+++ EnergyCalculator.java
@@ -1,8 +1,6 @@
 package physics;
 
-/**
- * Calculates energy loss when an object compresses a spring on different surfaces
- */
+
 public class EnergyCalculator {
     private final double initialEnergy;  // in joules
     private final double frictionlessCompression;  // in meters
@@ -15,24 +13,16 @@ public class EnergyCalculator {
         this.frictionlessCompression = frictionlessCompression;
         this.roughCompression = roughCompression;
     }
-    
-    /**
-     * Calculates the spring constant based on frictionless compression
-     */
+ 
     public double calculateSpringConstant() {
         return (2 * initialEnergy) / Math.pow(frictionlessCompression, 2);
     }
-    
-    /**
-     * Calculates energy lost due to friction on rough surface
-     */
+   
     public double calculateEnergyLost() {
         double k = calculateSpringConstant();
         double roughEnergy = 0.5 * k * Math.pow(roughCompression, 2);
         return initialEnergy - roughEnergy;
     }
-    
-    // Getters
     public double getInitialEnergy() { return initialEnergy; }
     public double getFrictionlessCompression() { return frictionlessCompression; }
     public double getRoughCompression() { return roughCompression; }
